4f0902810f4d259a6d2e696bd9c3cd05fb047063
David Scales <davidsscales@gmail.com>
2016-03-24 17:36:20 -0700

    Update: Update documentation, make small tweaks.

--- js/app.js
+++ js/app.js
@@ -23,7 +23,7 @@
  * put into the view. A Google Places request is then build based on our neighborhood, and sent.
  * The response is a list of objects, each representing a takeout restaurant. A callback function
  * converts each of these into a 'Place' object, and stores them in a 'places' list. Finally,
- * the next stage of the app is initiated with a call to 'getDetais'.
+ * the next stage of the app is initiated with a call to 'getDetails'.
  */
 
 // Initialize map
@@ -55,8 +55,11 @@ function initMap() {
 
 	// Prepare Google Places request
 	service = new google.maps.places.PlacesService(map);
+
 	// Custom timing measurement - Google Places request sent
 	window.performance.mark('places_request_start');
+	console.log('Requesting Google Places data...');
+
 	// Send Google Places request
 	service.textSearch(request, placesCallback);
 }
@@ -80,7 +83,7 @@ function placesCallback(results, status) {
 	    // Custom timing measurement - places list built
 		window.performance.mark('places_list_built');
 
-		// For testing
+		// For testing - TODO
 		console.log(results);
 
 		// Request more details for the Place objects from Google Places
@@ -89,34 +92,33 @@ function placesCallback(results, status) {
 }
 
 /*******************************************************************************************
- *
+ * Model
  *******************************************************************************************/
 
-/* Places are stored in an observable array, which is bound to the #places-list <ul>
- * in the HTML. This automatically updates the View with a list */
-var places = ko.observableArray();
-
-/* Results from the Google Places request are converted into a Place object, where each property
- * is an observable */
+/* Results from the Google Places request are converted into a Place object. Place objects
+ * store relevant data about each place, that will be displayed in the view. KO observables
+ * are used to ensure view content is updated when future API calls change model data.
+ *
+ * Because 3rd Party API's are called only once, I could likely avoid using KO observables
+ * in this section, in lieu of more traditional dynamic content creation. However, as KO
+ * is already loaded for other aspects of the app, I will stick to it for consistency, and
+ * potential future scalability */
 var Place = function(placeData) {
-	//
+
+	// Store local scope
 	var self = this;
-	// TODO
-	this.name = placeData.name; // The name of the place // Even need this line?
+
+	// These values are extracted from the initial Places request
+	this.name = placeData.name; // The name of the place
 	this.open = placeData.opening_hours.open_now; // TODO - show and/or filter by 'open now'
 	this.address = placeData.formatted_address; // Formatted address
-	this.shortAddress = placeData.formatted_address.split(',')[0];
-	// console.log (this.shortAddress); //TODO
-	this.place_id = placeData.place_id; // TODO
-	// TODO
-	this.google_rating = placeData.rating;
+	this.shortAddress = placeData.formatted_address.split(',')[0]; // Simple street address
+	this.place_id = placeData.place_id; // Identifier that will be used for future API requests
+	this.google_rating = placeData.rating; // Google's rating for this place
 
 	// Extract location
 	var lat = placeData.geometry.location.lat();
 	var lng = placeData.geometry.location.lng();
-	//
-	this.lat = lat;
-	this.lng = lng;
 
 	// Create and append Google map markers for each place based on location
 	this.marker = new google.maps.Marker({
@@ -126,68 +128,69 @@ var Place = function(placeData) {
 		animation: google.maps.Animation.DROP // Add drop animation for marker initialization
 	});
 
-	// --
-	// Added later from getDetails ..
-	this.formatted_phone_number = ko.observable('formatted phone number');
-	this.raw_phone_number = 'raw phone number';
-	// Added later from initYelp ..
-	this.review_count = '# of reviews';
-	this.yelp_rating_img_url = 'http://placekitten.com/g/50/50';
-	this.yelp_url = 'http://placekitten.com/g/200/150';
-	this.yelp_image_url = 'http://placekitten.com/g/200/150';
+	// These values will be updated on a successful request to Google Places Details
+	this.formatted_phone_number = ko.observable('Phone number unavailable'); // Formatted phone number
+	this.raw_phone_number = 'raw phone number'; // Unformatted phone number
 
-	// --
+	// These values will be updated on a successful request to Yelp
+	this.review_count = ko.observable('# of reviews unavailable'); // # of Yelp reviews
+	this.yelp_rating_img_url = ko.observable('http://placekitten.com/g/50/50'); // Yelp rating image
+	this.yelp_url = ko.observable('http://placekitten.com/g/200/150'); // Corresponding Yelp web page
+	this.yelp_image_url = ko.observable('http://placekitten.com/g/200/150'); // A Yelp image of the place
+
+	/* Using a KO computed observable, the content of Google Map info windows can be dynamically
+	 * updated as future API calls change Place data */
 	this.infoWindowContent = ko.computed(function(){
 		var contentString = '';
-		// --
+		// TODO
 		contentString += '<h3>'+self.formatted_phone_number()+'</h3>'+
-						 '<img src="'+self.yelp_image_url+'" alt="yelp rating">';
+						 '<img src="'+self.yelp_image_url()+'" alt="yelp rating">';
 
 		return contentString;
 	});
-	// --
+
+	// Info windows will be built on demand to ensure up-to-date values are used
 	this.buildInfoWindow = function(){
 		self.infoWindow = new google.maps.InfoWindow({
-				content: self.infoWindowContent() // -- ?
+				content: self.infoWindowContent()
 			});
 	};
 
 	// Listen to marker clicks
 	this.marker.addListener('click', function() {
-		// On click, set corresponding place to be the active place
+		// On click, set this place to be the "active" place
 		viewModel.setActivePlace(self);
 	});
 };
 
+/* Places are stored in an observable array, which is bound to the #places-list <ul>
+ * in the HTML. This automatically updates the View with a list */
+var places = ko.observableArray();
+
 /*******************************************************************************************
- *
+ * ViewModel
  *******************************************************************************************/
 
 // ViewModel allows interaction between UI/View and Model/data
 var ViewModel = function() {
 
-	// Save local scope
+	// Store local scope
 	var self = this;
 
-	// Text to filter our places is bound to <input>
+	// A bound text <input> allows filtering of places.
 	self.filterText = ko.observable('Pizza');
 
-	/* Filter places by user text input. This is bound to <ul> for places, and will update in real
-	 * time as the user changes the filter text in <input>. Updating/re-computation will not occur
-	 * if 'places' changes however, thanks to the use of peek(). This will prevent re-computation
-	 * each time a new place is added during 'places' initialization, as well as when additional data
-	 * from the Yelp API is added to each place. */
+	/* This is the list of places to be displayed in the view (as a list, and as markers on the map).
+	 * It is filtered by user text input. It's bound to the <ul> for places, and will update in real
+	 * time as the user changes the filter text in the <input>. */
 	self.filteredList = ko.computed(function(){
-
-		//
-		console.log('filtering');
 
 		// Custom timing measurement - filter start
 		window.performance.mark('filter_start');
 
 		// Store the filter text and the places list, limiting re-computation
 		var filterText = self.filterText().toLowerCase(); // Filter text, lower cased
-		var placesCopy = places.peek().slice(); // Places list. Peek() prevents recomputation
+		var placesCopy = places().slice(); // Places list
 		var placesLength = placesCopy.length; // Places list length, for loop
 		// Store filtered places
 		var filteredList = [];
@@ -210,118 +213,137 @@ var ViewModel = function() {
 
 		// Custom timing measurement - filter end
 		window.performance.mark('filter_end');
-		window.performance.measure('filter_time', 'filter_start', 'filter_end');
 
 		// Return only places that contain the filter text
 		return filteredList;
 	});
 
 	/* Keep track of which place is currently under investigation by the user, or "active".
-	 * A click listener exists on each WHAT UI element, that calls a function which changes
-	 * the clicked UI element's corresponding place to the "active" place. KO logic in the html
-	 * changes the CSS class of the active UI element.
-	 * Bound to WHAT */ // TODO - WHAT?
+	 * A click listener exists on each 'place' UI element (map marker and <li> item), that calls
+	 * a function which changes the clicked element's corresponding place to the "active" place.
+	 * KO logic in the html changes the CSS class of the active UI element.
+	 */
 	self.activePlace = ko.observable('uninitialized');
 
 	/* Update the place that is currently active.
-	 * Bound to WHAT, called on UI click. */ // TODO - WHAT?
+	 * Called on UI click ('place' map marker or <li> item */
 	self.setActivePlace = function(clickedPlace) {
 
 		// If the active place has been initialized
 		if (self.activePlace() !== 'uninitialized') {
 			// Reset the active place's map marker
 			self.activePlace().marker.setIcon(null);
-			// -- Close the active place's info window
+			// Close the active place's info window
 			self.activePlace().infoWindow.close();
-
 		}
 
 		// Set new active place
 		self.activePlace(clickedPlace);
 
-		// Set new marker icon
+		// Set new marker icon, which is distinct from the rest
 		self.activePlace().marker.setIcon('http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|00BCD4');
 
 		// Create a bounce animation for the active place
 		self.activePlace().marker.setAnimation(google.maps.Animation.BOUNCE); // Begin bouncing
 		setTimeout( function(){clickedPlace.marker.setAnimation(null)}, 1400 ); // Stop after 2 bounces (700ms each)
 
-		//-- Open info window
+		// Build and open info window
 		self.activePlace().buildInfoWindow();
 		self.activePlace().infoWindow.open(map, self.activePlace().marker);
 	}
 };
 
-// Apply bindgings to the ViewModel, linking UI/View with Model/data
+// Apply bindings to the ViewModel, linking UI/View with Model/data
 var viewModel = new ViewModel();
 ko.applyBindings(viewModel);
 
 /*******************************************************************************************
- *
+ * Google Places Details request
  *******************************************************************************************/
 
-/* */
+/* This function performs a second Google Places request, for each 'place' in the model.
+ * This second request gets additional information for each 'place', specifically its phone number.
+ * The phone number is used not only for the view, but also to identify corresponding location
+ * information returned from future Yelp requests. */
 function getDetails(){
 
+	console.log('Requesting Google Place Details data...');
+
+	// Keep track of when requests have been recieved for all places
 	var count = 0;
 
-	for (var i = 0; i < places().length; i++) {
+	// For each place in the model
+	for (var i = 0, total = places().length; i < total; i++) {
+
+		// Create a Google Place Details request object
 		var request = {
 			placeId: places()[i].place_id
 		};
+
+		// Create and send a Google Place Details request
 		service = new google.maps.places.PlacesService(map);
-		service.getDetails(request, callback);
+		service.getDetails(request, detailsCallback);
 	}
 
-	function callback(place, status){
+	// This callback recieves the new data for each place, and incorperates it into the model
+	function detailsCallback(place, status){
+
+		// If the request was successful
 		if (status == google.maps.places.PlacesServiceStatus.OK) {
 
-			for (var j = 0; j < places().length; j++){
+			// Compare each place in the model to the data recieved
+			for (var j = 0, total = places().length; j < total; j++){
+
+				// If the places have the same ID
 				if (place.place_id == places()[j].place_id){
-					// places()[j].formatted_phone_number = place.formatted_phone_number;
-					// --
+					// Update the corresponding place's phone number fields in the model
 					places()[j].formatted_phone_number(place.formatted_phone_number);
 					places()[j].raw_phone_number = place.formatted_phone_number.replace(/[()-]|\s/g, "");
 				}
-				else{
-					//
-				}
 			}
 
+			// Increment the number of successful responses
 			count++;
 
+			// If all of the responses were successful, and each place in the model has phone number data
 			if (count >= places().length){
+				// Initialize Yelp requests
 				initYelp();
 			}
 
 		}
+		// Otherwise the request was unsuccessful
 		else {
+			// Log status / error
 			console.log(status);
-			//
 		}
 	}
 }
 
 /*******************************************************************************************
- *
+ * Yelp requests
  *******************************************************************************************/
 
-// https://discussions.udacity.com/t/how-to-make-ajax-request-to-yelp-api/13699/4
-// MarkN
-
+/* This function is called once each place in the model has aquired phone number data from
+ * the Google Place Details request (in 'getDetails'). It iterates through each place in the
+ * model, and makes a request for Yelp data on each corresponding place. Once Yelp data is
+ * recieved, the model is updated */
 function initYelp() {
 
-	// Testing
-	console.log('Initializing Yelp...');
+	/* Thanks to MarkN on Udacity forums for help explaining Yelp request and OAuth use
+	 * https://discussions.udacity.com/t/how-to-make-ajax-request-to-yelp-api/13699/4 */
 
-	// Abstract some Yelp API parameters
+	console.log('Requesting Yelp data...');
+
+	// Abstract some Yelp API / OAuth parameters
+	// These should ideally be hidden somehow...
 	var yelp_url = 'https://api.yelp.com/v2/search?';
 	var YELP_KEY = 'c_mhQXy35rz4043INaHmfg';
 	var YELP_KEY_SECRET = 'GyunKRoE9EkuYwUe_zzFkJ24JG8';
 	var YELP_TOKEN = 'LtonUTLkQVzi-rA3HEp0rWvmLd9DuvTm';
 	var YELP_TOKEN_SECRET = '_AzbVT0ASeJhb1BT92FS1kofRk8';
 
-	//
+	// For each place in the model
 	for (var i = 0, total = places().length; i < total; i++) {
 
 		// Set required Yelp API parameters object
@@ -330,10 +352,10 @@ function initYelp() {
 			oauth_consumer_key: YELP_KEY,
 			oauth_token: YELP_TOKEN,
 			oauth_nonce: Math.floor(Math.random() * 1e12).toString(), // Generates a random number and returns it as a string for OAuthentication
-			oauth_timestamp: Math.floor(Date.now()/1000),
+			oauth_timestamp: Math.floor(Date.now()/1000), // Generates a timestamp
 			oauth_signature_method: 'HMAC-SHA1',
 			oauth_version : '1.0',
-			callback: 'yelpCB', // This is crucial to include for jsonp implementation in AJAX or else the oauth-signature will be wrong.
+			callback: 'yelpCallback', // This is crucial to include for jsonp implementation in AJAX or else the oauth-signature will be wrong.
 			// Place-specific parameters for request
 			location: places()[i].address, // Location of the place
 			term: places()[i].name, // Name of place
@@ -352,38 +374,54 @@ function initYelp() {
 			cache: true,  // This is crucial to include as well to prevent jQuery from adding on a cache-buster parameter "_=23489489749837", invalidating our oauth-signature
 			dataType: 'jsonp', // Need jsonp for cross-domain requests
 			jsonp: false, // Prevent jQuery from defining its own callback function, which invalidates OAuth stuff
-			test: 'test' // TODO
 		};
 
-		/* done and fail - jsonP */ // TODO
-
-		//
 		// Send AJAX query via jQuery library
 		$.ajax(settings);
-
 	}
 };
 
-function yelpCB(data) {
+// This callback recieves the Yelp data for each place, and incorperates it into the model.
+function yelpCallback(data) {
 
-	//
-	placesCopy = [];
-	for (var i = 0; i < places().length; i++) {
-		placesCopy.push(places()[i]);
-	}
-	placesLength = placesCopy.length;
+	// Compare each place in the model with the response data
+	for (var i = 0, total = places().length; i < total; i++) {
 
-	for (var i = 0; i < placesLength; i++) {
-		if ( placesCopy[i].raw_phone_number == data.businesses[0].phone || placesCopy[i].shortAddress == data.businesses[0].location.address[0]) {
+		// If the phone numbers or addresses match
+		if ( places()[i].raw_phone_number == data.businesses[0].phone || places()[i].shortAddress == data.businesses[0].location.address[0]) {
 
-			places()[i].review_count = data.businesses[0].review_count;
-			places()[i].yelp_rating_img_url = data.businesses[0].rating_img_url;
-			places()[i].yelp_url = data.businesses[0].url;
-			places()[i].yelp_image_url = data.businesses[0].image_url;
+			// Update model data
+			places()[i].review_count(data.businesses[0].review_count); // # of Yelp reviews
+			places()[i].yelp_rating_img_url(data.businesses[0].rating_img_url); // Yelp rating image
+			places()[i].yelp_url(data.businesses[0].url); // Corresponding Yelp web page
+			places()[i].yelp_image_url(data.businesses[0].image_url); // A Yelp image from the place
+			// End once a match is found
 			return;
 		}
 	}
 };
+
+/* A note on failure: This app performs four external API requests.
+ *
+ * The first is to Google Maps, to create the map and Google Places library. Should this fail, the
+ * entire app's functionality is essentially destroyed, since all features are based on this. There
+ * is thus no way to really compensate for this. Even if the app didn't break, there would be nothing
+ * to display.
+ *
+ * The second is to Google Places, to get data on 'places' in the neighborhood. Similarly, if this
+ * fails, the entire app's functionality is destroyed, and again there is thus no way to really
+ * compensate for this. Even if the app didn't break, there would be nothing to display.
+ *
+ * The third is to Google Place Details, which gets phone numbers for each place in the model. If this
+ * fails, the fourth request (to Yelp) would cause errors, because this request relies on comparing
+ * phone numbers in order to identify which place in the model to update. However, because the fourth
+ * request (to Yelp) is only executed if all Google Place Details requests succeed, no errors should
+ * occur. Failure should simply mean that default model values are not updated, and these default values
+ * specify to the user that an error has occured and the app will not break
+ *
+ * The fourth is to Yelp, and similarly to the Google Place Details request, failure of this request
+ * should simple mean that default model values aren't updated, and these value should inform the user
+ * that an error has occured, and the app will not break */
 
 
 
